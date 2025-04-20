4d85da6ec523cfe95f0ebdadef5126b2278cceb2
thuhuongit <hle183414@gmail.com>
2025-04-21 01:04:57 +0700

    web

--- src/migrations/migration-create-user.js
+++ src/migrations/migration-create-user.js
@@ -25,7 +25,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       phone:{
-        type: DataTypes.STRING
+        type: Sequelize.STRING
       },
       gender:{
         type: Sequelize.BOOLEAN
